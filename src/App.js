62977aeb6bc21e793b9065f921137aee5c9659bb
BenyaminB <60136796+BenyaminB@users.noreply.github.com>
2021-03-15 13:32:42 +0000

    New Version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+const api = {
+  key: "8067e7cdf802448391ee48b69e2dbc04",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
+  const [weather, setWeather] = useState({});
+
+  fetch(`${api.base}weather?q=${'London'}&units=metric&APPID=${api.key}`)
+  .then(res => res.json())
+  .then(result => {
+    setWeather(result);
+    console.log(result);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
+      <main>
+        {(typeof weather.main != "undefined") ? (
+        <div>
+          <div className="location-box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+          </div>
+          <div className="weather-box">
+            <div className="temp">
+              {Math.round(weather.main.temp)}°c
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        </div>
+        ) : ('')}
+      </main>
     </div>
   );
 }
