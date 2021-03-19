8544183a72ff3dadc1d630a4f03412870baa8092
saakshisaxena <saakshi.saxena2000@gmail.com>
2021-03-19 19:25:43 +0000

    Login
    Login and welcome page for admin user, in working condition with all css and js done.

--- src/App.js
+++ src/App.js
@@ -1,65 +1,45 @@
 import React, { useState } from 'react';
-const api = {
-  key: "6c91341b4c2a841ddae2e208e7da2de4",
-  base: "https://api.openweathermap.org/data/2.5/"
-}
+import LoginForm from './components/LoginForm';
 
 function App() {
-  const [query, setQuery] = useState('');
-  const [weather, setWeather] = useState({});
-
-  const search = evt => {
-    if (evt.key === "Enter") {
-      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-        .then(res => res.json())
-        .then(result => {
-          setWeather(result);
-          setQuery('');
-          console.log(result);
-        });
-    }
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin123"
   }
 
-  const dateBuilder = (d) => {
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const [user, setUser] = useState({name:"", email:""});
+  const [error, setError] = useState("");
 
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
+  const Login = details => {
 
-    return `${day} ${date} ${month} ${year}`
+    if (details.email == adminUser.email && details.password == adminUser.password) {
+      console.log("Logged In");
+      setUser({
+        name: details.name,
+        email: details.email
+      });
+    } else {
+      console.log("Details do not match");
+      setError("Details do not match");
+    }
+
+  }
+
+  const Logout = () => {
+    console.log("Logout");
+    setUser({name:"", email:""});
   }
 
   return (
-    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
-      <main>
-        <div className="search-box">
-          <input
-            type="text"
-            className="search-bar"
-            placeholder="Search..."
-            onChange={e => setQuery(e.target.value)}
-            value={query}
-            onKeyPress={search}
-          />
+    <div className="App">
+      {(user.email != "") ? (
+        <div className="welcome">
+          <h2> Welcome, <span>{user.name}</span></h2>
+          <button onClick={Logout}> Logout </button>
         </div>
-        {(typeof weather.main != "undefined") ? (
-        <div>
-          <div className="location-box">
-            <div className="location">{weather.name}, {weather.sys.country}</div>
-            <div className="date">{dateBuilder(new Date())}</div>
-          </div>
-          <div className="weather-box">
-            <div className="temp">
-              {Math.round(weather.main.temp)}°c
-            </div>
-            <div className="weather">{weather.weather[0].main}</div>
-          </div>
-        </div>
-        ) : ('')}
-      </main>
+      ) : (
+        <LoginForm  Login={Login} error={error}/>
+      )}
     </div>
   );
 }
