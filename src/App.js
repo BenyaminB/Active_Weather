352980e914d91c3ed41044e4eb124883d8a35d99
BenyaminB <60136796+BenyaminB@users.noreply.github.com>
2021-03-22 16:08:39 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 
   const search = evt => {
     if (evt.key === "Enter") {
-      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      fetch(`${api.base}weather?q=${'London'}&units=metric&APPID=${api.key}`)
         .then(res => res.json())
         .then(result => {
           setWeather(result);
@@ -19,7 +19,6 @@ function App() {
         });
     }
   }
-
 
   
 
