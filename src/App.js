9ec4ef7c44d16af1a127a673975051677a1e9e93
Carlos <c.a.naranjogomes@se19.qmul.ac.uk>
2021-03-22 14:39:28 +0000

    .
    .

--- src/App.js
+++ src/App.js
@@ -20,6 +20,8 @@ function App() {
     }
   }
 
+  #qwerty
+
   const dateBuilder = (d) => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
