2b8e4a481895e89777b968908e2c4e345dca14b0
BenyaminB <60136796+BenyaminB@users.noreply.github.com>
2021-03-26 16:00:45 +0000

    Updated - Merge

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import { currentWeatherIcon, currentHourlyIcon, TodaysTopSport, getDailyIcon, numberOneSuggested, numberTwoSuggested, numberThreeSuggested } from './common.js';
+import arrow from './assets/arrow.png';
 import styled from "styled-components";
 import React, { useState, useEffect } from 'react';
-import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Reports from './pages/Reports';
-import Products from './pages/Products';
+import { currentWeatherIcon, currentHourlyIcon, TodaysTopSport, getDailyIcon, numberOneSuggested, numberTwoSuggested, numberThreeSuggested } from './common.js';
 
 const locationApi = {
   key:"e18cd550-7ab3-11eb-b603-3d466becf114",
@@ -51,6 +48,7 @@ function App() {
   const [isOpen, setIsOpen] = useState(false);
   const [hourly, setHourly] = useState([]);
   const [daily, setDaily] = useState([]);
+  const [timezone, setTimezone] = useState("Europe/London");
 
   const fetchWeather = async () => {
 
@@ -93,6 +91,19 @@ function App() {
       });
   }
 
+  // used to get days of the week depending on the integer returned from the api 
+  const getTodaysDay = (d) => {
+    let days = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+    let day = days[d];
+    return `${day}`
+  }
+
+  // used to get the current time in the city searched in a 24 hour format 
+  const getCurrentTime = (t) => {
+    let hourTime = new Date(weather.dt * 1000).toLocaleTimeString("en-us", { hour: "numeric", hour12: false, timeZone: timezone });
+    return `${hourTime}`
+  }
+
   // const dateBuilder = (d) => {
   //   let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   //   let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -118,14 +129,6 @@ function App() {
     <AppContainer>
       <BoxContainer>
         <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
-          <Router>
-            <Navbar />
-            <Switch>
-              <Route path='/home' exact components={Home} />
-              <Route path='/reports' components={Reports} />
-              <Route path='/products' components={Products} />
-            </Switch>
-          </Router>
           <main>
             {/* -------------------------------------------- SEARCH BOX ------------------------------------------------------ */}
             <div className="search-box">
@@ -139,8 +142,9 @@ function App() {
 
               />
             </div>
-          {/* --------------------------- EVERYTHING BELOW IS DISPLAYED ONLY IF CITY IS SELECTED---------------------------------------- */}
-          {(typeof weather.main != "undefined") ? (
+
+            {/* --------------------------- EVERYTHING BELOW IS DISPLAYED ONLY IF CITY IS SELECTED---------------------------------------- */}
+            {(typeof weather.main != "undefined") ? (
               <div>
                 {/* div containing the city and country  */}
                 <div className="location-box">
@@ -151,38 +155,8 @@ function App() {
                 <div className="weather-box">
 
                   <div className="tempIcon" >
-                    {(weather.weather[0].id > 199 && weather.weather[0].id < 232) ? (
-                      <img className="tempIconSize"
-                        src={thunderstorm}
-                        alt='Icon'
-                      />
-                    ) : (weather.weather[0].id > 299 && weather.weather[0].id < 532) ? (
-                      <img className="tempIconSize"
-                        src={rain}
-                        alt='Icon'
-                      />
-                    ) : (weather.weather[0].id > 599 && weather.weather[0].id < 632) ? (
-                      <img className="tempIconSize"
-                        src={snow}
-                        alt='Icon'
-                      />
-                    ) : (weather.weather[0].id > 700 && weather.weather[0].id < 782) ? (
-                      <img className="tempIconSize"
-                        src={haze}
-                        alt='Icon'
-                      />
-                    ) : (weather.weather[0].id === 800) ? (
-                      <img className="tempIconSize"
-                        src={sunny}
-                        alt='Icon'
-                      />
-
-                    ) : (weather.weather[0].id > 800 && weather.weather[0].id < 805) ? (
-                      <img className="tempIconSize"
-                        src={cloud}
-                        alt='Icon'
-                      />
-                    ) : ('')}
+                    {/* calling an external function to get the weather icon  */}
+                    {currentWeatherIcon(weather, getCurrentTime(weather))}
 
                     {/* ----------------------------------------- TODAYS HIGH/LOW  ----------------------------------------- */}
                     <div className="minMax">H:{Math.round(weather.main.temp_max)} L:{Math.round(weather.main.temp_min)} </div>
@@ -191,26 +165,35 @@ function App() {
                   {/* ------------------------------------------- TODAYS TEMP  ------------------------------------------------ */}
                   <div className="temp">
                     {Math.round(weather.main.temp)}°C
-                {/* div holding todays weather description */}
+                    {/* div holding todays weather description */}
                     <div className="description">{weather.weather[0].main}</div>
+
                   </div>
 
                   {/* ------------------------------------------- HOURLY TEMP  ------------------------------------------------ */}
                   <div className="hourlyWeather">
 
                     {
-                      hourly.map(item => (
-                        // timeRN = new Date(item.dt * 1000)
-                        // let dateObj = new Date(item.dt * 1000);
-                        // let hours = dateObj.getUTCHours().toString();
-                        // return (
-                        //   <div>
+                      hourly.slice(0, 24).map(item => {
 
-                        < p > { Math.round(item.temp)}°C</p>
-                        // </div>
+                        let hour = new Date(item.dt * 1000).toLocaleTimeString("en-us", { hour: "numeric", hour12: true, timeZone: timezone });
+                        let eachHour24 = new Date(item.dt * 1000).toLocaleTimeString("en-us", { hour: "numeric", hour12: false, timeZone: timezone });
 
+                        return (
+                          // div for hourly weather
+                          <div className="eachHour">
 
-                      ))
+                            {/* displaying the hour  */}
+                            <p clasName="time">{hour}</p>
+
+                            {/* displaying the icon according to the weather id */}
+                            <p>{currentHourlyIcon(item, eachHour24, hour)}</p>
+
+                            {/* temp for each hour  */}
+                            <p className="tempHour">  {Math.round(item.temp)}°C</p>
+                          </div>
+                        )
+                      })
                     }
                   </div>
 
@@ -219,167 +202,67 @@ function App() {
                     <p className="topSportHeading">Top Suggested Sport</p>
                     {/* top sport displayed based on todays weather  */}
                     <div className="topIconContainer">
-                      {(weather.weather[0].main.includes('Clouds') || weather.weather[0].main.includes('Haze') || weather.weather[0].main.includes('Smoke')) ? (
-                        <div className="sportList">
-                          <img className="topSportIcon"
-                            src={football}
-                            alt='Icon'
-                          /> <p className="topSportName">Football</p>
-                        </div>
-                      ) : (weather.weather[0].main.includes('Clear')) ? (
-                        <div className="sportList">
-                          <img className="topSportIcon"
-                            src={swim}
-                            alt='Icon'
-                          /> <p className="topSportName">Swimming</p>
-                        </div>
-                      ) : (weather.weather[0].main.includes('Snow')) ? (
-                        <div className="sportList">
-                          <img className="topSportIcon"
-                            src={ski}
-                            alt='Icon'
-                          /><p className="topSportName">Skiing</p>
-                        </div>
-                      ) : (weather.weather[0].main.includes('Rain') || weather.weather[0].main.includes('Thunderstorm') || weather.weather[0].main.includes('Drizzle')) ? (
-                        <div className="sportList">
-                          <img className="topSportIcon"
-                            src={cycle}
-                            alt='Icon'
-                          /> <p className="topSportName">Cycle</p>
-                        </div>
-                      ) : ('')}
+                      {TodaysTopSport(weather)}
                     </div>
                   </div>
                 </div>
 
                 {/* ------------------------------------------- BOTTOM CONTAINER  ------------------------------------------------ */}
                 <div className="bottomContainer">
+
                   {/* daily weather displayed only when toggle is off*/}
                   {!isOpen && <div className="weekForecast">
 
                     {
-                      // console.log(daily)
-                      daily.map(item => (
-
-                        <p>{item.temp.day}</p>
-
-                      ))
+                      daily.map(item => {
+                        let n = new Date(item.dt * 1000).getUTCDay();
+                        return (
+                          <div className="eachDay">
+                            <p className="day">{getTodaysDay(n)}</p>
+                            <p>
+                              {/* getting daily icon through a function */}
+                              {getDailyIcon(item)}
+                            </p>
+                            <p className="todaysH">H:{Math.round(item.temp.max)}</p>
+                            <p className="todaysL">L:{Math.round(item.temp.min)}</p>
+                          </div>
+                        )
+                      })
                     }
-
-
                   </div>}
 
                   {/* ------------------------------------------ OTHER SPORT SUGGESTIONS ------------------------------------------------ */}
                   <div className="suggestions">
+                    {/* shows the below when the button is not clicked eg in the not opened state  */}
                     {!isOpen && <button className="toggleUp" onClick={() => setIsOpen(!isOpen)}><img className="arrowUp" src={arrow} alt="arrow" /></button>}
-                    {!isOpen && <div className="toggleHeadingBottom">Other suggested Sports</div>}
+                    {!isOpen && <div className="toggleHeadingBottom">Other Suggested Sports</div>}
+
+                    {/* shows the below when the button is clicked eg in the opened state  */}
                     {isOpen && <button className="toggleDown" onClick={() => setIsOpen(!isOpen)}><img className="arrowDown" src={arrow} alt="arrow" /></button>}
-                    {isOpen && <div className="toggleHeadingTop">Other suggested Sports</div>}
+                    {isOpen && <div className="toggleHeadingTop">Other Suggested Sports</div>}
+                    {isOpen && <hr className="HRLineTop" />}
 
                     {/* number 2 */}
                     {isOpen && <div>
-                      {(weather.weather[0].main.includes('Clouds') || weather.weather[0].main.includes('Haze') || weather.weather[0].main.includes('Smoke')) ? (
-                        <div className="sportList">
-                          <p className="sportName"><img className="sportIcon"
-                            src={basket}
-                            alt='Icon'
-                          /> Basketball</p>
-                        </div>
-                      ) : (weather.weather[0].main.includes('Clear')) ? (
-                        <div className="sportList">
-                          <p className="sportName"><img className="sportIcon"
-                            src={volley}
-                            alt='Icon'
-                          /> Volleyball</p>
-                        </div>
-                      ) : (weather.weather[0].main.includes('Snow')) ? (
-                        <div className="sportList">
-                          <p className="sportName"><img className="sportIcon"
-                            src={snowboard}
-                            alt='Icon'
-                          /> Snowboarding</p>
-                        </div>
-                      ) : (weather.weather[0].main.includes('Rain') || weather.weather[0].main.includes('Thunderstorm') || weather.weather[0].main.includes('Drizzle')) ? (
-                        <div className="sportList">
-                          <p className="sportName"><img className="sportIcon"
-                            src={football}
-                            alt='Icon'
-                          /> Indoor Football</p>
-                        </div>
-                      ) : ('')}
+                      {numberOneSuggested(weather)}
                     </div>}
                     {/* Number 3 */}
                     {isOpen && <div>
-                      {(weather.weather[0].main.includes('Clouds') || weather.weather[0].main.includes('Haze') || weather.weather[0].main.includes('Smoke')) ? (
-                        <div className="sportList">
-                          <p className="sportName"><img className="sportIcon"
-                            src={tennis}
-                            alt='Icon'
-                          /> Tennis</p>
-                        </div>
-                      ) : (weather.weather[0].main.includes('Clear')) ? (
-                        <div className="sportList">
-                          <p className="sportName"><img className="sportIcon"
-                            src={football}
-                            alt='Icon'
-                          /> Football</p>
-                        </div>
-                      ) : (weather.weather[0].main.includes('Snow')) ? (
-                        <div className="sportList">
-                          <p className="sportName"><img className="sportIcon"
-                            src={skate}
-                            alt='Icon'
-                          /> Ice Skating</p>
-                        </div>
-                      ) : (weather.weather[0].main.includes('Rain') || weather.weather[0].main.includes('Thunderstorm') || weather.weather[0].main.includes('Drizzle')) ? (
-                        <div className="sportList">
-                          <p className="sportName"><img className="sportIcon"
-                            src={volley}
-                            alt='Icon'
-                          /> Indoor Volleyball</p>
-                        </div>
-                      ) : ('')}
+                      {numberTwoSuggested(weather)}
                     </div>}
                     {/* Number 4 */}
                     {isOpen && <div>
-                      {(weather.weather[0].main.includes('Clouds') || weather.weather[0].main.includes('Haze') || weather.weather[0].main.includes('Smoke')) ? (
-                        <div className="sportList">
-                          <p className="sportName"><img className="sportIcon"
-                            src={run}
-                            alt='Icon'
-                          /> Running</p>
-                        </div>
-                      ) : (weather.weather[0].main.includes('Clear')) ? (
-                        <div className="sportList">
-                          <p className="sportName"><img className="sportIcon"
-                            src={tennis}
-                            alt='Icon'
-                          /> Tennis</p>
-                        </div>
-                      ) : (weather.weather[0].main.includes('Snow')) ? (
-                        <div className="sportList">
-                          <p className="sportName"><img className="sportIcon"
-                            src={sledge}
-                            alt='Icon'
-                          /> Sledging</p>
-                        </div>
-                      ) : (weather.weather[0].main.includes('Rain') || weather.weather[0].main.includes('Thunderstorm') || weather.weather[0].main.includes('Drizzle')) ? (
-                        <div className="sportList">
-                          <p className="sportName"><img className="sportIcon"
-                            src={run}
-                            alt='Icon'
-                          /> Running</p>
-                        </div>
-                      ) : ('')}
+                      {numberThreeSuggested(weather)}
                     </div>}
                   </div>
                 </div>
+
+                {/* used to create the wavy div which hold the bottom container */}
                 <svg>
                   <clipPath id="wave" clipPathUnits="objectBoundingBox">
                     <path className="st0" d="M1,0c0,0-0.3,0.1-0.5,0.1S0.3,0,0,0.1V1h1L1,0z" />
                   </clipPath>
                 </svg>
-
               </div>
             ) : ('')}
           </main>
